ca7b961a1f0736d028af466687a413461034228c
Anmol Ahuja <me@anmolahuja.com>
2017-04-02 16:23:19 +0000

    Add a serializer for tracked coords.

--- api/models.py
+++ api/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+from rest_framework import serializers
+
 
 class TrackedCoordinatePairs(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
@@ -14,14 +16,22 @@ class TrackedCoordinatePairs(models.Model):
     to_longitude = models.FloatField()
     to_latitude = models.FloatField()
 
-    def generate_id(self, from_latitude, from_longitude, to_latitude, to_longitude):
+    @classmethod
+    def generate_id(cls, from_latitude, from_longitude, to_latitude, to_longitude):
         id = '{},{},{},{}'.format(from_latitude, from_longitude,to_latitude, to_longitude)
         return id
 
-    def create_tracker(self, from_latitude, from_longitude, to_latitude, to_longitude, id=None):
+    @classmethod
+    def create_tracker(cls, from_latitude, from_longitude, to_latitude, to_longitude, id=None):
         if id is None:
-            id = self.generate_id(from_latitude, from_longitude, to_latitude, to_longitude)
-        self.objects.create(id=id, from_latitude=from_latitude, from_longitude=from_longitude,
+            id = cls.generate_id(from_latitude, from_longitude, to_latitude, to_longitude)
+        return cls.objects.create(id=id, from_latitude=from_latitude, from_longitude=from_longitude,
                             to_longitude=to_longitude, to_latitude=to_latitude)
     #class Meta:
     #    app_label = 'hermesapi'
+
+class TrackedCoordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrackedCoordinatePairs
+        fields = ('id', 'from_latitude', 'from_longitude', 'to_latitude', 'to_longitude')
+
